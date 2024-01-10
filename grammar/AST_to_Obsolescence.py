f524812d246fd9bef959a461733595fa0c926106
Ivan David ALFONSO DIAZ <ivandaalf@hotmail.com>
2024-01-10 15:00:26 +0100

    Modify obsolescence model generation

--- grammar/AST_to_Obsolescence.py
+++ grammar/AST_to_Obsolescence.py
@@ -3,41 +3,27 @@ from .ObsolescenceLexer import ObsolescenceLexer
 from .ObsolescenceParser import ObsolescenceParser
 from .ModelCreationListener import ModelCreationListener
 from BUML.metamodel.structural import DomainModel
-from metamodel import ObsolescenceRulesModel
+from metamodel import enable_obsolescence
 import os
 
-class Obsolescence:
-
-    def __init__(self, obsolescence_rules: str, buml_model: DomainModel):
-        self.obsolescence_rules: str = obsolescence_rules
-        self.buml_model: DomainModel = buml_model
-        self.obsolescence_model: ObsolescenceRulesModel = None
-
-    def generate_obsolescence_model(self):
-        lexer = ObsolescenceLexer(FileStream(self.obsolescence_rules))
-        parser = ObsolescenceParser(CommonTokenStream(lexer))
-        parse_tree = parser.obsolescence()
-        # file creation
-        if not os.path.exists("buml"):
-            os.makedirs("buml")
-        output = open("buml/obsol_model.py","w")
-        listen = ModelCreationListener(output)
-        walker = ParseTreeWalker()
-        walker.walk(listen, parse_tree)
-        output.close()
-        # model creation
-        namespace = {}
-        with open("buml/obsol_model.py", 'r') as obs_model:
-            code = obs_model.read()
-            exec(code, namespace)
-        function = namespace.get('create_model')
-        self.obsolescence_model = function(self.buml_model)
-        return self.obsolescence_model
-
-    @property
-    def obsolescence_model(self) -> ObsolescenceRulesModel:
-        return self.__obsolescence_model
-
-    @obsolescence_model.setter
-    def obsolescence_model(self, obsolescence_model: ObsolescenceRulesModel):
-        self.__obsolescence_model = obsolescence_model
+def obsolescence_declaration(obsolescence_rules: str, buml_model: DomainModel):
+    lexer = ObsolescenceLexer(FileStream(obsolescence_rules))
+    parser = ObsolescenceParser(CommonTokenStream(lexer))
+    parse_tree = parser.obsolescence()
+    # file creation
+    if not os.path.exists("buml"):
+        os.makedirs("buml")
+    output = open("buml/obsolescence_definition.py","w")
+    listen = ModelCreationListener(output)
+    walker = ParseTreeWalker()
+    walker.walk(listen, parse_tree)
+    output.close()
+    # model creation
+    namespace = {}
+    with open("buml/obsolescence_definition.py", 'r') as obs_model:
+        code = obs_model.read()
+        exec(code, namespace)
+    function = namespace.get('create_model')
+    obsolescence_model = function(buml_model)
+    enable_obsolescence()
+    return obsolescence_model
