f47e40ef32d94a8eb23a9f4aa65acba668229787
Ivan David ALFONSO DIAZ <ivandaalf@hotmail.com>
2024-01-17 16:41:11 +0100

    Update example

--- basic_example/obs.py
+++ basic_example/obs.py
@@ -1,12 +1,13 @@
 # Add Python Path
-import sys
-sys.path.append("../")
+#import sys
+#sys.path.append("../")
 
 from grammar import obsolescence_declaration
-from BUML.notations.plantUML import plantuml_to_buml
-from BUML.metamodel.structural import DomainModel, Class
+from besser.BUML.notations.plantUML import plantuml_to_buml
+from besser.BUML.metamodel.structural import DomainModel, Class
 from metamodel import ObsolescenceDeclaration, Change, Revision
 from runtime_engine import check_obsolescence
+import datetime
 
 #PlantUML to BUML using ANTLR
 modeltest: DomainModel = plantuml_to_buml(plantUML_model_path='target_model.plantuml')
@@ -14,10 +15,11 @@ modeltest: DomainModel = plantuml_to_buml(plantUML_model_path='target_model.plan
 # Obsolescence declaration
 obs_model: ObsolescenceDeclaration = obsolescence_declaration(obsolescence_rules="obsolescence_declaration.txt", buml_model=modeltest)
 
-# Add a change to the Book class
-book_change = Change(name="an attribute updated")
+# Add changes to the Book class
+change_1 = Change(name="Attribute updated", timestamp=datetime.datetime(2023, 12, 15))
+change_2 = Change(name="Class name updated", timestamp=datetime.datetime(2024, 1, 2))
 book = modeltest.get_class_by_name(class_name="Book")
-book.add_change(change=book_change)
+book.change_history = [change_1, change_2]
 
 # check model obsolescence
 check_obsolescence(obsolescence_declaration=obs_model, buml_model=obs_model)
