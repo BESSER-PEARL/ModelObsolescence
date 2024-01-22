f97cf3132f1c07e22ca7526d779d7d0cd43c383b
ivan-alfonso <ivandaalf@hotmail.com>
2024-01-22 11:47:04 +0100

    basic example modified

--- basic_example/obs.py
+++ basic_example/obs.py
@@ -7,7 +7,7 @@ from besser.BUML.notations.plantUML import plantuml_to_buml
 from besser.BUML.metamodel.structural import DomainModel, Class
 from metamodel import ObsolescenceDeclaration, Change, Revision
 from runtime_engine import check_obsolescence
-import datetime
+import datetime, time
 
 #PlantUML to BUML using ANTLR
 modeltest: DomainModel = plantuml_to_buml(plantUML_model_path='target_model.plantuml')
@@ -15,11 +15,26 @@ modeltest: DomainModel = plantuml_to_buml(plantUML_model_path='target_model.plan
 # Obsolescence declaration
 obs_model: ObsolescenceDeclaration = obsolescence_declaration(obsolescence_rules="obsolescence_declaration.txt", buml_model=modeltest)
 
-# Add changes to the Book class
-change_1 = Change(name="Attribute updated", timestamp=datetime.datetime(2023, 12, 15))
-change_2 = Change(name="Class name updated", timestamp=datetime.datetime(2024, 1, 2))
 book = modeltest.get_class_by_name(class_name="Book")
-book.change_history = [change_1, change_2]
 
 # check model obsolescence
-check_obsolescence(obsolescence_declaration=obs_model, buml_model=obs_model)
+time.sleep(3)
+check_obsolescence(obsolescence_declaration=obs_model)
+
+time.sleep(3)
+check_obsolescence(obsolescence_declaration=obs_model)
+
+
+'''
+for revision in book.revision_history:
+    print(revision)
+
+
+author = modeltest.get_class_by_name(class_name="Author")
+for revision in author.revision_history:
+    print (revision.comment + "   reviewer: " + revision.reviewer)
+print("Obsolescence author: " + str(author.obsolete))
+'''
+# Add revision
+#revision: Revision = Revision(name="Revision 1", reviewer="Manager1")
+#book.add_revision(revision)
