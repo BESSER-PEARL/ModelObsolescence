cad2b11a1ac509a8ed177f5d02801eed23566837
Ivan David ALFONSO DIAZ <ivandaalf@hotmail.com>
2024-02-06 18:05:27 +0100

    update basic example

--- basic_example/obs.py
+++ basic_example/obs.py
@@ -5,17 +5,18 @@
 from grammar import obsolescence_declaration
 from besser.BUML.notations.plantUML import plantuml_to_buml
 from besser.BUML.metamodel.structural import DomainModel, Class
-from metamodel import ObsolescenceDeclaration, Change, Revision
+from metamodel import ObsolescenceDeclaration, enable_obsolescence, Change, Revision
 from runtime_engine import check_obsolescence
 import datetime, time
 
-#PlantUML to BUML using ANTLR
+# PlantUML to BUML using ANTLR
 modeltest: DomainModel = plantuml_to_buml(plantUML_model_path='target_model.plantuml')
 
 # Obsolescence declaration
 obs_model: ObsolescenceDeclaration = obsolescence_declaration(obsolescence_rules="obsolescence_declaration.txt", buml_model=modeltest)
 
-book = modeltest.get_class_by_name(class_name="Book")
+# Extend the model with obsolescence attrs and methods
+enable_obsolescence(model=modeltest)
 
 # check model obsolescence
 time.sleep(3)
@@ -23,18 +24,3 @@ check_obsolescence(obsolescence_declaration=obs_model)
 
 time.sleep(3)
 check_obsolescence(obsolescence_declaration=obs_model)
-
-
-'''
-for revision in book.revision_history:
-    print(revision)
-
-
-author = modeltest.get_class_by_name(class_name="Author")
-for revision in author.revision_history:
-    print (revision.comment + "   reviewer: " + revision.reviewer)
-print("Obsolescence author: " + str(author.obsolete))
-'''
-# Add revision
-#revision: Revision = Revision(name="Revision 1", reviewer="Manager1")
-#book.add_revision(revision)
