5883b1b12bd0c5625d64a40b4abda327240fa392
ivan-alfonso <ivandaalf@hotmail.com>
2024-02-20 18:39:24 +0100

    update mobility model and rules

--- mobility_case/mobility_model.py
+++ mobility_case/mobility_model.py
@@ -1,13 +1,12 @@
-from besser.BUML.metamodel.structural import NamedElement, DomainModel, Type, Class, \
-        Property, PrimitiveDataType, Multiplicity, Association, BinaryAssociation, Generalization, \
-        GeneralizationSet, AssociationClass, EnumerationLiteral, Enumeration
-
+from besser.BUML.metamodel.structural import DomainModel, Class,  Property, PrimitiveDataType, \
+        Multiplicity, Association, BinaryAssociation, Generalization, EnumerationLiteral, Enumeration
 from besser.utilities import ModelSerializer
 
 # Primitive Data Types 
 int_type = PrimitiveDataType("int")
 float_type = PrimitiveDataType("float")
 str_type = PrimitiveDataType("str")
+time_type = PrimitiveDataType("time")
 
 # ChargingState Enum
 charging_state: Enumeration = Enumeration(name="ChargingState", literals={
@@ -28,74 +27,84 @@ station_type: Enumeration = Enumeration(name="StationType", literals={
     EnumerationLiteral(name="supercharger", owner=None)})
 
 # Segment class definition 
-Segment_slope: Property = Property(name="slope", property_type=int_type)
-Segment_distance: Property = Property(name="distance", property_type=float_type)
-Segment_coating: Property = Property(name="coating", property_type=coating_type)
-Segment_GeoPath: Property = Property(name="GeoPath", property_type=str_type)
-Segment: Class = Class(name="Segment", attributes={Segment_slope, Segment_distance, Segment_coating, Segment_GeoPath})
+segment_slope: Property = Property(name="slope", property_type=int_type)
+segment_distance: Property = Property(name="distance", property_type=float_type)
+segment_coating: Property = Property(name="coating", property_type=coating_type)
+segment_GeoPath: Property = Property(name="GeoPath", property_type=str_type)
+segment: Class = Class(name="Segment", attributes={segment_slope, segment_distance, segment_coating, segment_GeoPath})
 
 # Line class definition 
-Line_lineNumber: Property = Property(name="lineNumber", property_type=int_type)
-Line: Class = Class(name="Line", attributes={Line_lineNumber})
+line_lineNumber: Property = Property(name="lineNumber", property_type=int_type)
+line: Class = Class(name="Line", attributes={line_lineNumber})
 
 # ChargingPoint class definition 
-ChargingPoint_type: Property = Property(name="type", property_type=station_type)
-ChargingPoint_maxPower: Property = Property(name="maxPower", property_type=int_type)
-ChargingPoint_state: Property = Property(name="state", property_type=charging_state)
-ChargingPoint: Class = Class(name="ChargingPoint", attributes={ChargingPoint_type, ChargingPoint_maxPower, ChargingPoint_state})
+chargingPoint_type: Property = Property(name="type", property_type=station_type)
+chargingPoint_maxPower: Property = Property(name="maxPower", property_type=int_type)
+chargingPoint_state: Property = Property(name="state", property_type=charging_state)
+chargingPoint: Class = Class(name="ChargingPoint", attributes={chargingPoint_type, chargingPoint_maxPower, chargingPoint_state})
 
 # BusStop class definition 
-BusStop: Class = Class(name="BusStop", attributes=set())
+busStop: Class = Class(name="BusStop", attributes=set())
 
 # Road class definition 
-Road_name: Property = Property(name="name", property_type=str_type)
-Road: Class = Class(name="Road", attributes={Road_name})
+road_name: Property = Property(name="name", property_type=str_type)
+road: Class = Class(name="Road", attributes={road_name})
 
 # Vehicle class definition 
-Vehicle_brand: Property = Property(name="brand", property_type=str_type)
-Vehicle: Class = Class(name="Vehicle", attributes={Vehicle_brand})
+vehicle_brand: Property = Property(name="brand", property_type=str_type)
+vehicle: Class = Class(name="Vehicle", attributes={vehicle_brand})
+
+# Sensor class definition 
+sensor_temp: Property = Property(name="temperature", property_type=int_type)
+sensor_time: Property = Property(name="timeReach", property_type=time_type)
+sensor: Class = Class(name="Sensor", attributes={sensor_temp, sensor_time})
 
 # ElectricBus class definition 
-ElectricBus_batteryCapability: Property = Property(name="batteryCapability", property_type=int_type)
-ElectricBus: Class = Class(name="ElectricBus", attributes={ElectricBus_batteryCapability})
+electricBus_batteryCapability: Property = Property(name="batteryCapability", property_type=int_type)
+electricBus: Class = Class(name="ElectricBus", attributes={electricBus_batteryCapability})
 
 # Bus class definition 
-Bus_tankCapability: Property = Property(name="tankCapability", property_type=int_type)
-Bus: Class = Class(name="Bus", attributes={Bus_tankCapability})
+bus_tankCapability: Property = Property(name="tankCapability", property_type=int_type)
+bus: Class = Class(name="Bus", attributes={bus_tankCapability})
 
 # Point class definition 
 Point: Class = Class(name="Point", attributes={})
 
 # Relationships
 points: BinaryAssociation = BinaryAssociation(name="points", ends={
-        Property(name="points", property_type=Segment, multiplicity=Multiplicity(1, 1), is_navigable=False),
+        Property(name="points", property_type=segment, multiplicity=Multiplicity(1, 1), is_navigable=False),
         Property(name="points", property_type=Point, multiplicity=Multiplicity(2, 2), is_navigable=True)})
 segments: BinaryAssociation = BinaryAssociation(name="segments", ends={
         Property(name="segments", property_type=Point, multiplicity=Multiplicity(1, 1), is_navigable=False),
-        Property(name="segments", property_type=Segment, multiplicity=Multiplicity(1, "*"), is_navigable=True)})
+        Property(name="segments", property_type=segment, multiplicity=Multiplicity(1, "*"), is_navigable=True)})
 has_segments: BinaryAssociation = BinaryAssociation(name="has_segments", ends={
-        Property(name="has_segments", property_type=Line, multiplicity=Multiplicity(1, 1), is_navigable=False),
-        Property(name="has_segments", property_type=Segment, multiplicity=Multiplicity(2, "*"), is_navigable=True)})
+        Property(name="has_segments", property_type=line, multiplicity=Multiplicity(1, 1), is_navigable=False),
+        Property(name="has_segments", property_type=segment, multiplicity=Multiplicity(2, "*"), is_navigable=True)})
 has_points: BinaryAssociation = BinaryAssociation(name="has_points", ends={
-        Property(name="has_points", property_type=Line, multiplicity=Multiplicity(1, 1), is_navigable=False),
+        Property(name="has_points", property_type=line, multiplicity=Multiplicity(1, 1), is_navigable=False),
         Property(name="has_points", property_type=Point, multiplicity=Multiplicity(2, "*"), is_navigable=True)})
 lines: BinaryAssociation = BinaryAssociation(name="lines", ends={
-        Property(name="lines", property_type=BusStop, multiplicity=Multiplicity(1, 1), is_navigable=False),
-        Property(name="lines", property_type=Line, multiplicity=Multiplicity(1, "*"), is_navigable=True)})
+        Property(name="lines", property_type=busStop, multiplicity=Multiplicity(1, 1), is_navigable=False),
+        Property(name="lines", property_type=line, multiplicity=Multiplicity(1, "*"), is_navigable=True)})
 is_hosting: BinaryAssociation = BinaryAssociation(name="is_hosting", ends={
-        Property(name="is_hosting", property_type=BusStop, multiplicity=Multiplicity(0, 1), is_navigable=False),
-        Property(name="is_hosting", property_type=ChargingPoint, multiplicity=Multiplicity(1, "*"), is_navigable=True)})
+        Property(name="is_hosting", property_type=busStop, multiplicity=Multiplicity(0, 1), is_navigable=False),
+        Property(name="is_hosting", property_type=chargingPoint, multiplicity=Multiplicity(1, "*"), is_navigable=True)})
+sensor_measurments: Association = Association(name="measurements", ends={
+        Property(name="bus_stop", property_type=busStop, multiplicity=Multiplicity(0, "*"), is_navigable=True),
+        Property(name="bus", property_type=bus, multiplicity=Multiplicity(0, "*"), is_navigable=True),
+        Property(name="sensor", property_type=sensor, multiplicity=Multiplicity(1, "*"), is_navigable=True)})
+
 
 # Generalizations
-gen_Vehicle_ElectricBus: Generalization = Generalization(general=Vehicle, specific=ElectricBus)
-gen_Vehicle_Bus: Generalization = Generalization(general=Vehicle, specific=Bus)
-gen_Point_BusStop: Generalization = Generalization(general=Point, specific=BusStop)
-gen_Segment_Road: Generalization = Generalization(general=Segment, specific=Road)
+gen_Vehicle_ElectricBus: Generalization = Generalization(general=vehicle, specific=electricBus)
+gen_Vehicle_Bus: Generalization = Generalization(general=vehicle, specific=bus)
+gen_Point_BusStop: Generalization = Generalization(general=Point, specific=busStop)
+gen_Segment_Road: Generalization = Generalization(general=segment, specific=road)
 
 
 # Domain Model
-domain: DomainModel = DomainModel(name="mobility", types={Segment, Line, ChargingPoint, BusStop, Road, Vehicle, ElectricBus, Bus}, 
-                                  associations={points, segments, has_segments, has_points, lines, is_hosting}, 
+domain: DomainModel = DomainModel(name="mobility_model", types={segment, line, chargingPoint, busStop, road, vehicle, electricBus, bus, sensor}, 
+                                  associations={points, segments, has_segments, has_points, lines, is_hosting, sensor_measurments}, 
                                   generalizations={gen_Vehicle_ElectricBus, gen_Vehicle_Bus, gen_Point_BusStop, gen_Segment_Road},
                                   enumerations={charging_state, coating_type, station_type})
 
---
+++ mobility_case/obs/obsolescence_definition.py
@@ -0,0 +1,26 @@
+import datetime 
+from besser.BUML.metamodel.structural import DomainModel 
+from metamodel.obsolescence import ObsolescenceDeclaration, ObsolescenceRulesModel, Impact, \
+	FixedObsolescence, PeriodicObsolescence, InternalObsolescence, DataObsolescence, CriticalType 
+
+def create_model(buml_model: DomainModel): 
+	# Definition of the <<Req1>> obsolescence rule 
+	Req1_impact_1 = Impact(name="Req1_impact_1", elements={buml_model.get_class_by_name("Vehicle"), buml_model.get_class_by_name("Segment")}, impact=100, propagation_level=0, propagation_impact=0) 
+	Req1 = FixedObsolescence(name="Req1", criticality=CriticalType.Error, date_set=datetime.datetime.now(), date=datetime.datetime(2024, 8, 1), impacts={Req1_impact_1}) 
+
+	# Definition of the <<Req2>> obsolescence rule 
+	Req2_impact_1 = Impact(name="Req2_impact_1", elements={buml_model.get_class_by_name("ChargingPoint")}, impact=10, propagation_level=1, propagation_impact=50) 
+	Req2 = PeriodicObsolescence(name="Req2", criticality=CriticalType.Warning, date_set=datetime.datetime.now(), periodicity=1, unit="m", impacts={Req2_impact_1}) 
+
+	# Definition of the <<Req3>> obsolescence rule 
+	Req3_impact_1 = Impact(name="Req3_impact_1", elements={buml_model.get_class_by_name("Sensor")}, impact=100, propagation_level=0, propagation_impact=0) 
+	Req3 = DataObsolescence(name="Req3", criticality=CriticalType.Error, date_set=datetime.datetime.now(), discrepancy=20, impacts={Req3_impact_1}) 
+
+	# Definition of the <<Req4>> obsolescence rule 
+	Req4_impact_1 = Impact(name="Req4_impact_1", elements={buml_model.get_class_by_name("BusStop")}, impact=100, propagation_level=0, propagation_impact=0) 
+	Req4 = InternalObsolescence(name="Req4", criticality=CriticalType.Warning, date_set=datetime.datetime.now(), rule="self.allClasses()->select(c | c.name.contains('Bus'))->union(self.allAttributes()->select(a | a.name.contains('bus')))", impacts={Req4_impact_1}) 
+
+	# Definition of the Obsolescence Model 
+	obsolescence_model = ObsolescenceRulesModel(name="mobility_model_obs", obs_declarations={Req1, Req2, Req3, Req4})
+
+	return obsolescence_model
