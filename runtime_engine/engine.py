224a6fa7f8ebf436d7247c38eb609492f49503c0
ivan-alfonso <ivandaalf@hotmail.com>
2024-02-20 18:39:51 +0100

    Add pdf report generation

--- runtime_engine/engine.py
+++ runtime_engine/engine.py
@@ -3,46 +3,57 @@ from dateutil.relativedelta import relativedelta
 from metamodel import ObsolescenceDeclaration, FixedObsolescence, PeriodicObsolescence
 from besser.BUML.metamodel.structural import DomainModel, NamedElement
 from metamodel import Revision, CriticalType
+from runtime_engine.report import Report
 
-def check_obsolescence(obsolescence_declaration: ObsolescenceDeclaration):
+#def check_obsolescence(obsolescence_declaration: ObsolescenceDeclaration):
+def check_obsolescence(obsolescence_declaration: ObsolescenceDeclaration, date=datetime.datetime.now()):
+    report: Report = Report()
     for rule in obsolescence_declaration.obs_declarations:
         if type(rule) == FixedObsolescence and rule.active == True:
-            check_temporal_fixed(rule)
+            check_temporal_fixed(rule, report, date)
         if type(rule) == PeriodicObsolescence and rule.active == True:
-            check_temporal_periodic(rule)
+            check_temporal_periodic(rule, report, date)
+    report.generate_report("report.pdf", obsolescence_declaration.name, date=date)
 
-def check_temporal_fixed(rule: FixedObsolescence):
-    if rule.date <= datetime.datetime.now():
+def check_temporal_fixed(rule: FixedObsolescence, report, date):
+    #if rule.date <= datetime.datetime.now():
+    if rule.date <= date:
         for impact in rule.impacts:
             for element in impact.elements:
                 element.obsolete = 100
-                create_revision(element=element, impact=100)
-                alert(element=element, criticality=rule.criticality, rule=rule)
                 propagate_obsolscence(element=element, propagation_level=impact.propagation_level, impact=100, 
-                                          p_impact_loss=impact.propagation_impact, criticality=rule.criticality, rule=rule)
+                                          p_impact_loss=impact.propagation_impact, criticality=rule.criticality, 
+                                          rule=rule, report=report)
+                create_revision(element=element, impact=100, date=date)
+                alert(element=element, criticality=rule.criticality, rule=rule, report=report)
                 rule.active = False
                 
-def check_temporal_periodic(rule: PeriodicObsolescence):
+def check_temporal_periodic(rule: PeriodicObsolescence, report, date):
     periodicity = convert_to_datetime(value=rule.periodicity, unit=rule.unit)
     for impact in rule.impacts:
         for element in impact.elements:
             last_revision = element.revision_history[-1].timestamp
-            if (datetime.datetime.now() - periodicity >= last_revision):
+            #if (datetime.datetime.now() - periodicity >= last_revision):
+            if (date - periodicity >= last_revision):
                 if element.obsolete + impact.impact > 100:
                     element.obsolete = 100
                 else:
                     element.obsolete += impact.impact
-                create_revision(element=element, impact=impact.impact)
-                alert(element=element, criticality=rule.criticality, rule=rule)
                 propagate_obsolscence(element=element, propagation_level=impact.propagation_level, impact=impact.impact, 
-                                          p_impact_loss=impact.propagation_impact, criticality=rule.criticality, rule=rule)
+                                          p_impact_loss=impact.propagation_impact, criticality=rule.criticality, 
+                                          rule=rule, report=report)
+                create_revision(element=element, impact=impact.impact, date=date)
+                alert(element=element, criticality=rule.criticality, rule=rule, report=report)
 
-def alert(element: NamedElement, criticality: CriticalType, rule: ObsolescenceDeclaration):
-    time_now = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S.%f")[:-6]
+def alert(element: NamedElement, criticality: CriticalType, rule: ObsolescenceDeclaration, report):
     if element.obsolete >= 100:
-        print(rule.name + "; \t" + time_now + "; \t" + "\033[91m" + element.name + " is obsolete; \t Criticality: " + criticality.value + "\033[0m")
+        alert = [element.name, "The class is obsolete", rule.name, criticality.value]
+        report.add_obsolete(alert)
+        
     else:
-        print(rule.name + "; \t" + time_now + "; \t" + element.name + " obsolescence increased to " + str(element.obsolete) + "%")
+        alert = [element.name, "Obsolescence of the class increased to " + str(element.obsolete) + "%", rule.name, criticality.value]
+        report.add_alert(alert)
+    print(alert)
 
 def convert_to_datetime(value, unit):
     unit_mapping = {
@@ -60,13 +71,14 @@ def convert_to_datetime(value, unit):
     time_period = unit_mapping[unit]
     return time_period
 
-def create_revision(element: NamedElement, impact: float):
+def create_revision(element: NamedElement, impact: float, date):
     comment = "Obsolescence of the element " + element.name + " increased by " + str(impact)
-    revision: Revision = Revision(name="Obsolescence revision", reviewer="runtime_engine", comment=comment)
+    #revision: Revision = Revision(name="Obsolescence revision", reviewer="runtime_engine", comment=comment)
+    revision: Revision = Revision(name="Obsolescence revision", reviewer="runtime_engine", comment=comment, timestamp=date)
     element.add_revision(revision)
 
 def propagate_obsolscence(element: NamedElement, propagation_level: int, impact: float, p_impact_loss: float, 
-                          criticality: CriticalType, rule: ObsolescenceDeclaration):
+                          criticality: CriticalType, rule: ObsolescenceDeclaration, report):
     propagation_impact = impact * (p_impact_loss / 100)
     elems = set()
     layers = dict()
@@ -86,4 +98,4 @@ def propagate_obsolscence(element: NamedElement, propagation_level: int, impact:
             impacted_el.obsolete = 100
         else:
             impacted_el.obsolete += propagation_impact
-        alert(impacted_el, criticality=criticality, rule=rule)
+        alert(impacted_el, criticality=criticality, rule=rule, report=report)
---
+++ runtime_engine/report.py
@@ -0,0 +1,80 @@
+from reportlab.lib import colors
+from reportlab.lib.pagesizes import letter
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Table, TableStyle
+from reportlab.lib.styles import getSampleStyleSheet
+
+class Report:
+    def __init__(self) -> None:
+        self.alerts = [["Model element", "Description", "Triggered rule", "Criticality"]]
+        self.obsoletes = [["Model element", "Description", "Triggered rule", "Criticality"]]
+
+    @property
+    def alerts(self):
+        return self.__alerts
+
+    @alerts.setter
+    def alerts(self, alerts):
+        self.__alerts = alerts
+
+    def add_alert(self, alert):
+        self.alerts.append(alert)
+
+    @property
+    def obsoletes(self):
+        return self.__obsoletes
+
+    @obsoletes.setter
+    def obsoletes(self, obsoletes):
+        self.__obsoletes = obsoletes
+
+    def add_obsolete(self, obsolete):
+        self.obsoletes.append(obsolete)
+    
+    def generate_report(self, file_name, model_name, date):
+        doc = SimpleDocTemplate(file_name, pagesize=letter)
+        
+        # PDF content
+        elements = []
+        
+        # Tittle
+        styles = getSampleStyleSheet()
+        title = Paragraph("Obsolescence Report <br/><br/>", styles['Title'])
+        elements.append(title)
+        
+        # Description
+        model = Paragraph("<b>Model:</b> " + model_name, styles['Normal'])
+        elements.append(model)
+        datetime_str = date.strftime("%Y-%m-%d %H:%M:%S")
+        date = Paragraph("<b>Date:</b> " + datetime_str +"<br/><br/>", styles['Normal'])
+        elements.append(date)
+        
+        # Create table with elements obsoletes
+        legendT1 = Paragraph("<u>Obsolete Model elements</u>:<br/><br/>", styles['Normal'])
+        elements.append(legendT1)
+        table_obsolete = Table(self.obsoletes)
+        elements.append(table_obsolete)
+        
+        
+        # Create table with elements gaining obsolescence
+        legendT2 = Paragraph("<br/><br/>Model elements experiencing obsolescence:<br/><br/>", styles['Normal'])
+        elements.append(legendT2)
+        table_alerts = Table(self.alerts)
+        elements.append(table_alerts)
+
+        # Table style
+        style = TableStyle([('BACKGROUND', (0,0), (-1,0), colors.Color(0.16, 0.43, 0.51)),
+                            ('TEXTCOLOR', (0,0), (-1,0), colors.whitesmoke),
+                            ('ALIGN', (0,0), (-1,-1), 'LEFT'),
+                            ('FONTNAME', (0,0), (-1,0), 'Helvetica-Bold'),
+                            ('BOTTOMPADDING', (0,0), (-1,0), 12),
+                            ('BACKGROUND', (0,1), (-1,-1), colors.Color(0.87, 0.92, 0.92)),
+                            ('GRID', (0,0), (-1,-1), 1, colors.transparent),
+                            ('LINEABOVE', (0,1), (-1,-1), 0, colors.white)])
+        
+        table_obsolete.setStyle(style)
+        table_obsolete._argW = [100, 230, 80, 70]
+        table_alerts.setStyle(style)
+        table_alerts._argW = [100, 230, 80, 70]
+
+        # Generate PDF
+        doc.build(elements)
