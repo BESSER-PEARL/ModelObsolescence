596a330aacab6ebb57f23c1779c1be839522efd2
ivan-alfonso <ivandaalf@hotmail.com>
2024-06-20 09:43:44 +0200

    Updating ObsolescenceRules parsing

--- mobility_case/mobility.py
+++ mobility_case/mobility.py
@@ -1,15 +1,19 @@
 import time, datetime
 from besser.BUML.metamodel.structural import DomainModel
 from besser.utilities import ModelSerializer
-from metamodel import ObsolescenceDeclaration
+from metamodel import ObsolescenceDeclaration, enable_obsolescence
 from runtime_engine import check_obsolescence
 from grammar import obsolescence_declaration
 
 serializer: ModelSerializer = ModelSerializer()
 mobility_model: DomainModel = serializer.load(model_path="mobility.buml")
 
+# Extend the model with obsolescence attrs and methods
+startdate = datetime.datetime(2024, 2, 1)
+enable_obsolescence(model=mobility_model, date=startdate)
+
 # Obsolescence declaration
-obs_model: ObsolescenceDeclaration = obsolescence_declaration(obsolescence_rules="obsolescence_rules.txt", domain_model=mobility_model, date=datetime.datetime(2024, 2, 1))
+obs_model: ObsolescenceDeclaration = obsolescence_declaration(obsolescence_rules="obsolescence_rules.txt", domain_model=mobility_model)
 
 # check model obsolescence
 check_obsolescence(obsolescence_declaration=obs_model, date=datetime.datetime(2024, 3, 1))
