2497c4f9a904132f8878487226bbe432a35a13c9
ivan-alfonso <ivandaalf@hotmail.com>
2024-06-28 11:01:57 +0200

    Update mobility model

--- mobility_case/mobility_model.py
+++ mobility_case/mobility_model.py
@@ -27,72 +27,66 @@ station_type: Enumeration = Enumeration(name="StationType", literals={
     EnumerationLiteral(name="supercharger", owner=None)})
 
 # Segment class definition 
-segment_slope: Property = Property(name="slope", property_type=int_type)
-segment_distance: Property = Property(name="distance", property_type=float_type)
-segment_coating: Property = Property(name="coating", property_type=coating_type)
-segment_GeoPath: Property = Property(name="GeoPath", property_type=str_type)
+segment_slope: Property = Property(name="slope", type=int_type)
+segment_distance: Property = Property(name="distance", type=float_type)
+segment_coating: Property = Property(name="coating", type=coating_type)
+segment_GeoPath: Property = Property(name="GeoPath", type=str_type)
 segment: Class = Class(name="Segment", attributes={segment_slope, segment_distance, segment_coating, segment_GeoPath})
 
-# Line class definition 
-line_lineNumber: Property = Property(name="lineNumber", property_type=int_type)
-line: Class = Class(name="Line", attributes={line_lineNumber})
+# BusLine class definition 
+line_lineNumber: Property = Property(name="lineNumber", type=int_type)
+busline: Class = Class(name="BusLine", attributes={line_lineNumber})
 
 # ChargingPoint class definition 
-chargingPoint_type: Property = Property(name="type", property_type=station_type)
-chargingPoint_maxPower: Property = Property(name="maxPower", property_type=int_type)
-chargingPoint_state: Property = Property(name="state", property_type=charging_state)
+chargingPoint_type: Property = Property(name="type", type=station_type)
+chargingPoint_maxPower: Property = Property(name="maxPower", type=int_type)
+chargingPoint_state: Property = Property(name="state", type=charging_state)
 chargingPoint: Class = Class(name="ChargingPoint", attributes={chargingPoint_type, chargingPoint_maxPower, chargingPoint_state})
 
 # BusStop class definition 
 busStop: Class = Class(name="BusStop", attributes=set())
 
 # Road class definition 
-road_name: Property = Property(name="name", property_type=str_type)
+road_name: Property = Property(name="name", type=str_type)
 road: Class = Class(name="Road", attributes={road_name})
 
 # Vehicle class definition 
-vehicle_brand: Property = Property(name="brand", property_type=str_type)
+vehicle_brand: Property = Property(name="brand", type=str_type)
 vehicle: Class = Class(name="Vehicle", attributes={vehicle_brand})
 
 # Sensor class definition 
-sensor_temp: Property = Property(name="temperature", property_type=int_type)
-sensor_time: Property = Property(name="timeReach", property_type=time_type)
+sensor_temp: Property = Property(name="temperature", type=int_type)
+sensor_time: Property = Property(name="timeReach", type=time_type)
 sensor: Class = Class(name="Sensor", attributes={sensor_temp, sensor_time})
 
 # ElectricBus class definition 
-electricBus_batteryCapability: Property = Property(name="batteryCapability", property_type=int_type)
+electricBus_batteryCapability: Property = Property(name="batteryCapability", type=int_type)
 electricBus: Class = Class(name="ElectricBus", attributes={electricBus_batteryCapability})
 
 # Bus class definition 
-bus_tankCapability: Property = Property(name="tankCapability", property_type=int_type)
+bus_tankCapability: Property = Property(name="tankCapability", type=int_type)
 bus: Class = Class(name="Bus", attributes={bus_tankCapability})
 
 # Point class definition 
 Point: Class = Class(name="Point", attributes={})
 
 # Relationships
-points: BinaryAssociation = BinaryAssociation(name="points", ends={
-        Property(name="points", property_type=segment, multiplicity=Multiplicity(1, 1), is_navigable=False),
-        Property(name="points", property_type=Point, multiplicity=Multiplicity(2, 2), is_navigable=True)})
-segments: BinaryAssociation = BinaryAssociation(name="segments", ends={
-        Property(name="segments", property_type=Point, multiplicity=Multiplicity(1, 1), is_navigable=False),
-        Property(name="segments", property_type=segment, multiplicity=Multiplicity(1, "*"), is_navigable=True)})
-has_segments: BinaryAssociation = BinaryAssociation(name="has_segments", ends={
-        Property(name="has_segments", property_type=line, multiplicity=Multiplicity(1, 1), is_navigable=False),
-        Property(name="has_segments", property_type=segment, multiplicity=Multiplicity(2, "*"), is_navigable=True)})
-has_points: BinaryAssociation = BinaryAssociation(name="has_points", ends={
-        Property(name="has_points", property_type=line, multiplicity=Multiplicity(1, 1), is_navigable=False),
-        Property(name="has_points", property_type=Point, multiplicity=Multiplicity(2, "*"), is_navigable=True)})
-lines: BinaryAssociation = BinaryAssociation(name="lines", ends={
-        Property(name="lines", property_type=busStop, multiplicity=Multiplicity(1, 1), is_navigable=False),
-        Property(name="lines", property_type=line, multiplicity=Multiplicity(1, "*"), is_navigable=True)})
+ends: BinaryAssociation = BinaryAssociation(name="ends", ends={
+        Property(name="ends", type=Point, multiplicity=Multiplicity(1, 1)),
+        Property(name="segments", type=segment, multiplicity=Multiplicity(1, "*"))})
+routes: BinaryAssociation = BinaryAssociation(name="routes", ends={
+        Property(name="has_segments", type=busline, multiplicity=Multiplicity(0, "*")),
+        Property(name="routes", type=segment, multiplicity=Multiplicity(1, "*"))})
+route: BinaryAssociation = BinaryAssociation(name="route", ends={
+        Property(name="route", type=busline, multiplicity=Multiplicity(1, 1)),
+        Property(name="bus", type=bus, multiplicity=Multiplicity(1, "*"))})
 is_hosting: BinaryAssociation = BinaryAssociation(name="is_hosting", ends={
-        Property(name="is_hosting", property_type=busStop, multiplicity=Multiplicity(0, 1), is_navigable=False),
-        Property(name="is_hosting", property_type=chargingPoint, multiplicity=Multiplicity(1, "*"), is_navigable=True)})
+        Property(name="is_hosting", type=busStop, multiplicity=Multiplicity(0, 1)),
+        Property(name="is_hosting", type=chargingPoint, multiplicity=Multiplicity(1, "*"))})
 sensor_measurments: Association = Association(name="measurements", ends={
-        Property(name="bus_stop", property_type=busStop, multiplicity=Multiplicity(0, "*"), is_navigable=True),
-        Property(name="bus", property_type=bus, multiplicity=Multiplicity(0, "*"), is_navigable=True),
-        Property(name="sensor", property_type=sensor, multiplicity=Multiplicity(1, "*"), is_navigable=True)})
+        Property(name="bus_stop", type=busStop, multiplicity=Multiplicity(0, "*")),
+        Property(name="bus", type=bus, multiplicity=Multiplicity(0, "*")),
+        Property(name="sensor", type=sensor, multiplicity=Multiplicity(1, "*"))})
 
 
 # Generalizations
@@ -103,8 +97,8 @@ gen_Segment_Road: Generalization = Generalization(general=segment, specific=road
 
 
 # Domain Model
-domain: DomainModel = DomainModel(name="mobility_model", types={segment, line, chargingPoint, busStop, road, vehicle, electricBus, bus, sensor}, 
-                                  associations={points, segments, has_segments, has_points, lines, is_hosting, sensor_measurments}, 
+domain: DomainModel = DomainModel(name="mobility_model", types={segment, busline, chargingPoint, busStop, road, vehicle, electricBus, bus, sensor}, 
+                                  associations={ends, routes, route, is_hosting, sensor_measurments}, 
                                   generalizations={gen_Vehicle_ElectricBus, gen_Vehicle_Bus, gen_Point_BusStop, gen_Segment_Road},
                                   enumerations={charging_state, coating_type, station_type})
 
